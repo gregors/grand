327d862b6dff5a630f067d206eaec7c26dccc9fa
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2017-05-28 19:29:50 -0400

    return matrix vectors by default, add zeros, add shape, keep old function as array_randn

--- lib/grand.rb
+++ lib/grand.rb
@@ -4,21 +4,28 @@ require 'matrix'
 require 'core_ext/enumerable'
 
 module GRand
+  def self.array_randn(*d)
+    return normal_distribution.rng if d.size == 0
+
+    x  = d.shift
+    x.times.map{ self.array_randn(*d) }
+  end
+
   def self.randn(*d)
     return normal_distribution.rng if d.size == 0
 
     x  = d.shift
-    x.times.map{ self.randn(*d) }
-  end
-
-  def self.vrandn(*d)
-    return normal_distribution.rng if d.size == 0
-
-    x  = d.shift
-    Vector[*x.times.map{ self.vrandn(*d) }]
+    Vector[*x.times.map{ self.randn(*d) }]
   end
 
   def self.normal_distribution
     @@nd ||= Rubystats::NormalDistribution.new(0, 1)
+  end
+
+  def self.zeros(*d)
+    return 0 if d.size == 0
+
+    x = d.shift
+    Vector[*x.times.map{ self.zeros(*d) } ]
   end
 end
--- spec/grand_spec.rb
+++ spec/grand_spec.rb
@@ -13,16 +13,45 @@ RSpec.describe GRand do
    end
 
    context 'with multiple args' do
+     it 'returns a vector' do
+       expect(GRand::randn(1,1).class).to eq Vector
+     end
+
+     it 'returns nested vectors' do
+       results = GRand::randn(2,1)
+       expect(results[0].class).to eq Vector
+       expect(results[1].class).to eq Vector
+     end
+   end
+  end
+
+  describe '.array_randn' do
+   context 'with no args' do
+     it 'returns a float' do
+       expect(GRand::array_randn.class).to eq  Float
+     end
+   end
+
+   context 'with multiple args' do
      it 'returns an array' do
-       expect(GRand::randn(1,1).class).to eq Array
+       expect(GRand::array_randn(1,1).class).to eq Array
      end
 
      it 'returns nested arrays' do
-       results = GRand::randn(2,1)
+       results = GRand::array_randn(2,1)
        expect(results[0].class).to eq Array
        expect(results[1].class).to eq Array
      end
    end
+  end
+
+  describe '.zeros' do
+    it 'returns nested Vector full of zeros' do
+       results = GRand::zeros(2,1)
+       expect(results[0].class).to eq Vector
+       expect(results[0][0]).to eq 0
+       expect(results[1][0]).to eq 0
+    end
   end
 
   describe 'monkey patches Enumerable' do
